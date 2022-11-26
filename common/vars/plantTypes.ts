71785f15616193177f327d43ba98883b32f47705
KaylaKZJ <kaylajoubert33@gmail.com>
2022-11-26 16:44:03 +0200

    Adds plant obj

--- common/vars/plantTypes.ts
+++ common/vars/plantTypes.ts
@@ -1,7 +1,7 @@
 export const plants = [
   { type: 'Unknown', waterFrequency: 5 },
-  { type: 'Bonsai', waterFrequency: 5 },
-  { type: 'Cactus', waterFrequency: 5 },
-  { type: 'Carnivorous', waterFrequency: 5 },
-  { type: 'Herb', waterFrequency: 5 },
+  { type: 'Bonsai', waterFrequency: 6 },
+  { type: 'Cactus', waterFrequency: 12 },
+  { type: 'Carnivorous', waterFrequency: 12 },
+  { type: 'Herb', waterFrequency: 7 },
 ];
